90ac1cb98d0d8b7bd3a6beb7946f79aadc3e1595
wesley <wesley@localhost.localdomain>
2019-03-26 18:44:57 +0700

    My forth commit

---
+++ hienthiJavascript/hienthiJavascript.js
@@ -0,0 +1,22 @@
+
+function showMessage(){
+    alert('Xin chào!');
+}
+
+
+
+function showMessage1(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+
+
+
+function showMessage2(){
+    document.write('Xin chào!');
+}
+
+
+
+function showMessage3(){
+    console.log('Xin chào!');
+}
