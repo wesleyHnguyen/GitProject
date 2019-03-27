3fc7882292d3a3542ca5a9d1a548e497dc3d4e98
wesleyHnguyen <root@localhost.localdomain>
2019-03-27 15:02:08 +0700

    the nineth commit

---
+++ calculator/calculator.js
@@ -0,0 +1,14 @@
+
+
+
+function  display(e) {
+    document.getElementById("expression").value = document.getElementById("expression").value +
+        "" + document.getElementById(e.id).value;
+}
+
+
+function result() {
+    let expressionString = document.getElementById("expression").value;
+    let expressionVal = eval(expressionString);
+    document.getElementById("expression").value = expressionVal;
+}
