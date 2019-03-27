8558257d1f692ef8a52953744b754f25f0691525
wesleyHnguyen <root@localhost.localdomain>
2019-03-27 09:27:41 +0700

    the fifth commit

---
+++ thucHanhbien/thucHanhbien.js
@@ -0,0 +1,14 @@
+
+let i = 10
+let f = 20.5
+let b = true
+let s = "Ha Noi"
+
+
+document.write("i = " + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
---
+++ thucHanhbien/thucHanhbienbai3.js
@@ -0,0 +1,6 @@
+let a = prompt("nhap gia tri cho a")
+let b = prompt("nhap gia tri cho b")
+
+if (a % b == 0) alert("a la boi so cua b")
+else
+    alert("a khong la boi so cua b")
