55758f3b4dfa403233ef036ad383c60f0b716a33
wesleyHnguyen <root@localhost.localdomain>
2019-04-09 08:29:50 +0700

    temperature converter

--- temperatureClass/main.js
+++ temperatureClass/main.js
@@ -1,4 +1,6 @@
-let temperature = new Temperature(25);
+let temp = Number(prompt("Enter a value in C temperature"));
+
+let temperature = new Temperature(temp);
 
 alert("Nhiet do 25 do C o nhiet do F co gia tri la: " +  temperature.cToFConverter());
 alert("Nhiet do 25 do C o nhiet do K co gia tri la: " +  temperature.cToKConverter());
